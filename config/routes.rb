191a71797146d53deb55e8bbb248dce5fb73d86a
MayuFujimoto <mayufujimoto41@gmail.com>
2025-07-15 20:32:05 +0900

    add:ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+   get "sessions/omniauth"
+   get "sessions/create"
+   get "sessions/failure"
+   # Google認証を開始するためのルーティング
+   get '/auth/:provider', to: 'sessions#omniauth'
+   # Googleからの認証コールバックを受け取るルーティング
+   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+   # 認証失敗時のリダイレクト先
+   match '/auth/failure', to: redirect('/'), via: [:get, :post]
+   # ログアウトのルーティング（POSTメソッドでCSRF対策）
+   post 'logout', to: 'sessions#destroy', as: :logout
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
