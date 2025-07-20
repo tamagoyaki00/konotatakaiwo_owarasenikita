d96fbbf7144dfd1a82094dc8bab2375586ba2ab6
MayuFujimoto <mayufujimoto41@gmail.com>
2025-07-20 13:56:26 +0900

    add:userモデルのRSpec追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :user do
+        sequence(:name) { |n| "Test User #{n}" }
+        sequence(:email) { |n| "test_#{n}@example.com"}
+        sequence(:uid) { |n| "uid_#{n}" }
+        provider { "test_provider" }
+    end
+end
