f7ac39946bded76b2fd7c37eac8dcbae5f7eef0e
MayuFujimoto <mayufujimoto41@gmail.com>
2025-07-22 21:04:31 +0900

    add:ユーザー名を編集可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,28 @@
 class UsersController < ApplicationController
   before_action :require_login
   before_action :set_user
-  
+
   def show
-    
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      flash.now[:notice] = "名前を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_user
     @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
---
+++ app/javascript/controllers/flash_message_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["container"]
+
+  connect() {
+    setTimeout(() => {
+      this.show()
+    }, 10);
+
+
+    this.timer = setTimeout(() => {
+      this.hide()
+    }, 10000); // 5秒後に消える (調整可能)
+  }
+
+  show() {
+    this.containerTarget.classList.remove("translate-x-full", "opacity-0");
+    this.containerTarget.classList.add("translate-x-0", "opacity-100");
+    this.containerTarget.classList.remove("pointer-events-none");
+    this.containerTarget.classList.add("pointer-events-auto");
+  }
+
+  hide() {
+    clearTimeout(this.timer);
+    this.containerTarget.classList.remove("translate-x-0", "opacity-100");
+    this.containerTarget.classList.add("translate-x-full", "opacity-0");
+    // pointer-events を none にして、非表示になった要素がクリックを妨げないようにする
+    this.containerTarget.classList.remove("pointer-events-auto");
+    this.containerTarget.classList.add("pointer-events-none");
+  }
+
+  // アニメーション終了後に要素をDOMから削除
+  remove(event) {
+    if (event.propertyName === 'transform' || event.propertyName === 'opacity') {
+        this.element.remove();
+    }
+  }
+
+  // disconnect はタイマーのクリーンアップに必要
+  disconnect() {
+    clearTimeout(this.timer);
+  }
+}
