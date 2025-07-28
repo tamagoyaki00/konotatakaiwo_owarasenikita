fc41b8e4a5eb9bbc007af56d1d5b574005a3f71f
MayuFujimoto <mayufujimoto41@gmail.com>
2025-07-28 11:36:57 +0900

    ci:メモリ設定・環境判定を追加

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,14 @@ Capybara.register_driver :remote_chrome do |app|
   options.add_argument('no-sandbox')
   options.add_argument('headless')
   options.add_argument('disable-gpu')
+  options.add_argument('--disable-dev-shm-usage') 
   options.add_argument('window-size=1680,1050')
   Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
+end
+
+if ENV['GITHUB_ACTIONS']
+  Capybara.default_driver = :remote_chrome
+  Capybara.javascript_driver = :remote_chrome
+  Capybara.server_host = '0.0.0.0'
+  Capybara.server_port = 3000
 end
