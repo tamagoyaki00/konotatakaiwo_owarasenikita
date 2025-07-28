796ac7a1a05520ddc4f29857b841ee25bf4bf1a2
MayuFujimoto <mayufujimoto41@gmail.com>
2025-07-28 11:50:04 +0900

    ci:fix:ポート番号変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,7 +3,7 @@ Capybara.register_driver :remote_chrome do |app|
   options.add_argument('no-sandbox')
   options.add_argument('headless')
   options.add_argument('disable-gpu')
-  options.add_argument('--disable-dev-shm-usage') 
+  options.add_argument('--disable-dev-shm-usage')
   options.add_argument('window-size=1680,1050')
   Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
 end
