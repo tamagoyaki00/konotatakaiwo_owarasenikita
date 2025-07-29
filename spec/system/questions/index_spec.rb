58a319e5575c24875bddabd2dbff9b40ad61ce2a
MayuFujimoto <mayufujimoto41@gmail.com>
2025-07-29 15:38:34 +0900

    add:問題詳細画面のテスト通過

---
+++ spec/system/questions/index_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "Questions index page", type: :system do
+
+  describe '一覧ページの表示' do
+    context '問題が存在する場合' do
+
+        let!(:user)  { create(:user) }
+        let!(:question1) { create(:question, title: '究極の選択1', user: user) }
+        let!(:question2) { create(:question, title: '究極の選択2', user: user) }
+
+        before do
+            visit questions_path
+        end
+
+        it 'すべての問題が表示される' do
+            expect(page).to have_content('究極の選択1')
+            expect(page).to have_content('究極の選択2')
+        end
+
+        it '問題ごとに、タイトル、作者の名前、アイコンが表示されること' do
+            within(".card-animated", text: question1.title) do
+                expect(page).to have_content(question1.title)
+                expect(page).to have_content("#{question1.user.name}")
+                expect(page).to have_selector("img[src='#{question1.user.google_image_url}']")
+            end
+        end
+    end
+
+    context 'お題が存在しない場合' do
+        before do
+            visit questions_path
+        end
+
+        it 'お題はありませんという文字が表示されること' do
+            expect(page).to have_content 'お題はありません'
+            expect(page).not_to have_selector('card-animated')
+        end
+
+    end
+  end
+end
---
+++ spec/system/questions/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Questions show page", type: :system do
+  let!(:user)  { create(:user) }
+  let!(:question) { create(:question, :with_options, user: user) }
+
+  describe 'お題詳細ページの表示' do
+    context '一覧画面から詳細画面へ移動する場合' do
+      it '詳細画面に正しく遷移し、内容が表示されること' do
+        visit questions_path
+        find(".card-animated", text: question.title).click
+        expect(page).to have_content(question.title)
+        expect(page).to have_content('どちらか選択してください')
+        expect(page).to have_current_path(question_path(question))
+        expect(page).to have_content(question.options.first.content)
+        expect(page).to have_content(question.options.last.content)
+        expect(page).to have_content("#{question.user.name}")
+        expect(page).to have_selector("img[src='#{question.user.google_image_url}']")
+      end
+    end
+
+  end
+end
