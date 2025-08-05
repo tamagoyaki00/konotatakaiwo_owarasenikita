968632e1aee66bb703e08d00a4665b021a9407da
MayuFujimoto <mayufujimoto41@gmail.com>
2025-08-05 16:16:35 +0900

    add:モデルファイル追加

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,2 +1,7 @@
 class Opinion < ApplicationRecord
+  belongs_to :user
+  belongs_to :question
+  belongs_to :option, optional: true
+
+  validates :content, presence: true, length: { maximum: 250 }
 end
