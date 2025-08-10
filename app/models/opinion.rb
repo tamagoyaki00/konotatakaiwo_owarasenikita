ac8734e99273bb3e692d74aa8901cc3322533761
MayuFujimoto <mayufujimoto41@gmail.com>
2025-08-10 22:46:34 +0900

    add:opinionモデルファイルに一部追加

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,7 +1,7 @@
 class Opinion < ApplicationRecord
   belongs_to :user
   belongs_to :question
-  has_many :opinion_reactions
+  has_many :opinion_reactions, dependent: :destroy
 
   validates :content, presence: true, length: { maximum: 250 }
   validates :user_id, presence: true
