cf81cacc5f76977c1dd8070ca251b2aac3cde7f3
MayuFujimoto <mayufujimoto41@gmail.com>
2025-08-19 16:11:25 +0900

    add:マイページにセレクトボックス追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,16 @@ class UsersController < ApplicationController
   before_action :correct_user, only: %i[ show edit update]
 
 def show
-  @questions = @user.questions.includes(:user).order(created_at: :desc)
+  @questions = case params[:sort_by]
+               when 'most_voted'
+                 @user.questions.most_voted
+               when 'most_opinions'
+                 @user.questions.most_opinions
+               when 'oldest'
+                 @user.questions.oldest
+               else
+                 @user.questions.order(created_at: :desc)
+               end
 end
 
   def edit
